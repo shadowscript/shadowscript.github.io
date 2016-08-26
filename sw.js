7f6726b08277366b59ad7198f2d201962270e005
Eric Caleb <calebme@mail.com>
2016-08-26 19:40:13 +0100

    Added function to delete old service workers

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
+// Register service worker and files to be cached
 self.addEventListener('install', function(e) {
 e.waitUntil(
-caches.open('shadowscript').then(function(cache) {
+caches.open('shadowscript2').then(function(cache) {
 return cache.addAll([
 './',
 './index.html',
@@ -19,10 +20,29 @@ return cache.addAll([
 });
 
 self.addEventListener('fetch', function(event) {
-console.log(event.request.url);
+	console.log(event.request.url);
 event.respondWith(
 caches.match(event.request).then(function(response) {
 return response || fetch(event.request);
 })
 );
+});
+
+
+// Delete old service workers
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['shadowscript2'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
